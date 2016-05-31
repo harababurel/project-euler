2b54b6aafa29567ad7268b5cf690ed2a80e5df1f
Sergiu Puscas <srg.pscs@gmail.com>
2016-05-31 11:40:55 +0300

    solved 10

---
+++ 10/main.hs
@@ -0,0 +1,7 @@
+prime :: (Integral a, Eq a) => a -> Bool
+prime 1 = False
+prime n = null [x | x <- [2..floor (sqrt (fromIntegral n))], mod n x == 0]
+
+primes = [x | x <- [1..2*10^6], prime x]
+
+main = print (sum primes)
