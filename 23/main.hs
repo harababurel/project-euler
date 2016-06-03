0a19ba97ba0958dda52a0970c5823828011cf27e
Sergiu Puscas <srg.pscs@gmail.com>
2016-06-03 21:00:44 +0300

    23 runs in 2.8 seconds now

--- 23/main.hs
+++ 23/main.hs
@@ -1,16 +1,11 @@
-sumOfDivisors :: Int -> Int
-sumOfDivisors x = xs !! x
-    where xs = map (\n -> 1 - (extra n) + sum [y + div n y | y <- [2..root n], mod n y == 0]) [0..]
-          root :: Int -> Int
+import Data.List
+import Data.IntSet (toList, fromList)
+
+writable = toList $ fromList [x + y | x <- xs, y <- xs, x <= y, x + y <= 28123]
+    where xs = filter (\n -> n < sumOfDivisors n) [2..28123]
+          sumOfDivisors n = sum [y + div n y | y <- [2..root n], mod n y == 0] - extra n
           root n = floor $ sqrt $ fromIntegral n
           extra n = if (root n)^2 == n then (root n)
                                        else 0
-abundant :: Int -> Bool
-abundant n = (sumOfDivisors n) > n
-
-notWritable :: Int -> Bool
-notWritable n = null [1 | x <- [1..div n 2], abundant x, abundant (n-x)]
-
-sol = filter notWritable [1..28123]
-
-main = print $ show sol ++ " " ++ show (sum sol)
+sol  = (div (28123 * 28124) 2) - sum writable
+main = print $ sol
