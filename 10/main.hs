2d2e745999903c78f37b4e8d0e9e77c0f6aa88d7
Sergiu Puscas <srg.pscs@gmail.com>
2016-06-16 14:35:42 +0300

    improved 10

--- 10/main.hs
+++ 10/main.hs
@@ -1,7 +1,11 @@
+{-
 prime :: (Integral a, Eq a) => a -> Bool
 prime 1 = False
 prime n = null [x | x <- [2..floor (sqrt (fromIntegral n))], mod n x == 0]
 
 primes = [x | x <- [1..2*10^6], prime x]
+-}
 
-main = print (sum primes)
+import Data.Numbers.Primes
+
+main = print $ sum $ takeWhile(<=2*10^6) primes
